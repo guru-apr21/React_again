61ead4abf0f16a9608d188a049bbbf338b176951
Guru Prakash Ram Balamurugan <guru.apr21@gmail.com>
2023-06-02 14:27:15 +0100

    Move nav elements to top right

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css"; // import the styles
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/contact">Contact</Link>
+    <nav className={styles.navbar}>
+      <Link href="/about" className={styles.navbar_link}>
+        About
+      </Link>
+      <Link href="/projects" className={styles.navbar_link}>
+        Projects
+      </Link>
+      <Link href="/contact" className={styles.navbar_link}>
+        Contact
+      </Link>
     </nav>
   );
 }
